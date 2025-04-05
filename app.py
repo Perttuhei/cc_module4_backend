0a5826ac2429dd6bd9e89013a9fc339a47bec153
Perttu Heikkala <perttu.heikkala@edu.lapinamk.fi>
2025-04-05 22:48:56 +0300

    2.1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p> Hello world from cc backend! version 2<p>"
+    return "<p> Hello world from cc backend! version 2.1<p>"
 
 @app.route('/data', methods=['POST'])
 def get_data():
